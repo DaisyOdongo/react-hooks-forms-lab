29126e8753528097fddd8c2ebf8510e5df5825ca
+ <+>
2022-09-27 14:52:20 +0300

    assignment completed

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, entry, onSearchChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input 
+      type="text" 
+      name="search" 
+      placeholder="Search..."  
+      value={entry} onChange={(e) =>onSearchChange(e.currentTarget.value)}/>
+      <select name="filter" onChange={onSearchChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,12 +3,17 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onAddElements }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm ] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  function handleSearchChange(userInput){
+      setSearchTerm(userInput)
+  }
+  
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -16,10 +21,14 @@ function ShoppingList({ items }) {
     return item.category === selectedCategory;
   });
 
+  function handleNewItemSubmit(data){
+    onAddElements(data)
+  }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleNewItemSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} entry={searchTerm} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
